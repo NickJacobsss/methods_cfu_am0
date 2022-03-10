cbbeb9366cb191bf8ffda85c8a36533d99bc4e9f
Nick Jacobs <nickjacobsatc@gmail.com>
2022-03-10 09:47:27 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -7,17 +7,27 @@
 # The return value is "HI"
 "Hello World".upcase
 
-
+#This is the include method being used on the string "hello world"
+#This passes the arguement "Hello" to the include method, which checks to see if the original string includes "Hello"
+# Return value is True
 "Hello World".include?("Hello")
 
-
+#This is the end_with method being used on the string "Hello World"
+#The passes the arguement "Hello" on the original string to test if "Hello World" ends with "Hello"
+#The return value is "false"
 "Hello World".end_with?("Hello")
 
-
+#This is the end_with method being used again on "Hello World"
+#This time, the arguement is "rld" on the string "Hello World"
+#Return result is "True"
 "Hello World".end_with?("rld")
 
-
+#This is the length method being used on the string "Hello World"
+#There is no argument in this code, it is only checking how long the string is
+#The output is 11 because there is 11 characters including the space
 "Hello World".length
 
-
+#This is the size method being used on the string "Hello World"
+#There is no argument, only checking the size of the string its attached to
+#The output is also 11
 "Hello World".size
